318d1ab78b2885d57d58b23fa19d7e0e43518c25
unknown <cosacog@gmail.com>
2020-05-18 20:59:29 +0900

    initial commit

---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public GameObject Cube;
+    Vector3 rotAngle = new Vector3(0.5f, 1.0f, 1.0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cube.transform.Rotate(rotAngle);
+    }
+}
